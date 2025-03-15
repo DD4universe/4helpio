b20ee2c30e6087cc7b302c58cc21254764aebbd9
DD4universe <itsdurai4@gmail.com>
2025-03-15 22:45:21 +0530

    Update helpio.js

--- helpio.js
+++ helpio.js
@@ -1,6 +1,6 @@
 document.addEventListener('contextmenu', (e) => e.preventDefault());
      document.addEventListener('keydown', (e) => {
-       if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+       if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')|| (e.ctrlKey && e.key==='U')) {
          e.preventDefault();
        }
      });
